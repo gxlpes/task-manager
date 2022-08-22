230fe4577025060105bf4b423cd3856ce5e38a4c
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-22 12:02:58 -0300

    Methods of module

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,29 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("get all items");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({}); // get the module and its methods
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body); // get the module and its methods
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    res.json({ id: req.params.id });
+  } catch {}
 };
 
 const updateTask = (req, res) => {
